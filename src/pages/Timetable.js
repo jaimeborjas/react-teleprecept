107a9b45f5c273ebeb283344494111445a0a83fe
cavenderj <cavenderj@duq.edu>
2022-04-24 18:52:02 -0400

    hour logging fixed

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -4,21 +4,14 @@ import { useMutation, useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import endPoints from 'services/api';
+//import { DatePicker } from '@mantine/dates';
 import { CounterClockwiseClockIcon } from '@modulz/radix-icons';
 
 const Timetable = () => {
-  
-  const useStyles = createStyles((theme) => ({
-      rowSelected: {
-        backgroundColor:
-          theme.colorScheme === 'dark'
-            ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
-            : theme.colors[theme.primaryColor][0],
-      },
-    }));
-
-    const TableSelection = () => {
-      const [data, setData] = useState([{"hours": 3,
+  function timeout(delay: number) {
+    return new Promise( res => setTimeout(res, delay) );
+  }
+  const [data, setData] = useState([{"hours": 3,
                                         "date": "12/31/2022",
                                         "verified": true,
                                         "id": '1'},
@@ -30,6 +23,18 @@ const Timetable = () => {
                                         "date": "12/31/2022",
                                         "verified": false,
                                         "id": '3'}])
+  const useStyles = createStyles((theme) => ({
+      rowSelected: {
+        backgroundColor:
+          theme.colorScheme === 'dark'
+            ? theme.fn.rgba(theme.colors[theme.primaryColor][7], 0.2)
+            : theme.colors[theme.primaryColor][0],
+      },
+    }));
+  
+    const TableSelection = (timetable) => {
+      //console.log(timetable.timetable)
+      setData(timetable.timetable)
       const { classes, cx } = useStyles();
       const [selection, setSelection] = useState(['1']);
       const toggleRow = (id: string) =>
@@ -50,7 +55,7 @@ const Timetable = () => {
                 transitionDuration={0}
               />
             </td>
-            <td>{item.date}</td>
+            <td>{item.date.substring(0,10)}</td>
             <td>{item.hours}</td>
           </tr>
         );
@@ -78,6 +83,7 @@ const Timetable = () => {
         </ScrollArea>
       );
     }
+    const [display, setDisplay] = useState(false)
     const { id } = useParams();
     const hoursRef = useRef(null);
     const dateRef = useRef(null);
@@ -90,43 +96,52 @@ const Timetable = () => {
     } = useQuery('timesheet', async () => {
       axios.defaults.headers.api = 123;
       axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-      const { data } = await axios.get(endPoints.base + `/timesheet/10`);
-      console.log(id);
+      const { data } = await axios.get(endPoints.base + `/timesheet/${id}`);
+      //console.log(id);
       return data;
     });
-    console.log(timetable)
-
+    //console.log(timetable)
     const submitHandler = async (event) => {
       event.preventDefault();
       logPost();
     };
-
+    const displayTable = () =>
+    {
+      setDisplay(true)
+    };
     const mutation = useMutation((log) => {
       axios.defaults.headers.api = 123;
       axios.defaults.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
-      return axios.patch(endPoints.base + `/timesheet/add/${id}`, log);
+      return axios.post(endPoints.base + `/timesheet/${id}`, log);
     });
 
     async function logPost () {
       const data = {
+          connectionId: id,
           hours: hoursRef.current.value,
           date: dateRef.current.value,
           validated: false
       };
+      //console.log(data)
+      hoursRef.current.value = "";
+      dateRef.current.value = "";
       mutation.mutate(data, {
         onSuccess: () => {
           refetch();
         },
       });
     };
-    return (
+    if(display === true)
+    {
+      //console.log(timetable[0].timesheet)
+      return (
     <div className="w-full flex justify-left">
       <div className="hidden md:block md:w-2/3">
-        <TableSelection></TableSelection>
-        <form action=''>
+        <TableSelection timetable={timetable[0].timesheet}></TableSelection>
+        <form onSubmit={submitHandler}>
           <TextInput
             ref={dateRef}
-            placeholder="Date"
+            placeholder="YYYY-MM-DD"
             label="Date"
             required
           />
@@ -143,6 +158,36 @@ const Timetable = () => {
       </div>
     </div>
     )
+    }
+    else
+    {
+    return (
+    <div className="w-full flex justify-left">
+      <div className="hidden md:block md:w-2/3">
+        <Button type="button" onClick={displayTable}>
+          Show Table
+        </Button>
+        <form onSubmit={submitHandler}>
+          <TextInput
+            ref={dateRef}
+            placeholder="YYYY-MM-DD"
+            label="Date"
+            required
+          />
+          <TextInput
+            ref={hoursRef}
+            placeholder="Hours"
+            label="Hours"
+            required
+          />
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>
+      </div>
+    </div>
+    )
+  }
 };
     
 export default Timetable;
