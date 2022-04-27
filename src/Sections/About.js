87acf9b852b2b5236a841542729f6eb4b159496f
JaredKonop <61473095+JaredKonop@users.noreply.github.com>
2022-04-27 19:43:57 -0400

    Update About

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -10,27 +10,27 @@ const About = () => {
       <Border />
 
       <div className="mt-12">
-        <Title className="text-3xl title about">Why Teleprecept-Match?</Title>
+        <Title className=" text-center text-stone-900">Why Teleprecept-Match?</Title>
       </div>
       <div className="mt-12 items-center">
         <Grid justify="space-between" align="center" style={{ textAlign: 'center' }}>
           <Grid.Col span={3} className="">
-            <SecurityIcon fill="#0646d1" />
+            <SecurityIcon fill="#34d399" />
             <Title className="text-xl title w-full">Security</Title>
             <Text className="text-md text pr-8 pl-8">HIPAA compliant. We believe in privacy and security.</Text>
           </Grid.Col>
           <Grid.Col span={3}>
-            <UsageIcon fill="#0646d1" />
+            <UsageIcon fill="#34d399" />
             <Title className="text-xl title">Ease of Use</Title>
             <Text className="text-md text pr-8 pl-8">No downloads required. Cross-platform support. We believe in simplicity.</Text>
           </Grid.Col>
           <Grid.Col span={3}>
-            <AccessibilityIcon fill="#0646d1" />
+            <AccessibilityIcon fill="#34d399" />
             <Title className="text-xl title">Accessibility</Title>
             <Text className="text-md text pr-8 pl-8">Meets ADA requirements. Designed with Accessibility in mind.</Text>
           </Grid.Col>
           <Grid.Col span={3}>
-            <CoverageIcon fill="#0646d1" />
+            <CoverageIcon fill="#34d399" />
             <Title className="text-xl title">Coverage</Title>
             <Text className="text-md text pr-8 pl-8">Our product is intented to provide service to students and preceptors nationwide.</Text>
           </Grid.Col>
@@ -41,3 +41,4 @@ const About = () => {
 };
 
 export default About;
+
