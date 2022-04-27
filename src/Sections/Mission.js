e113664c2f7aaed6083d13e6ef642608c0e3e343
JaredKonop <61473095+JaredKonop@users.noreply.github.com>
2022-04-27 19:42:17 -0400

    Update Mission

--- src/Sections/Mission.js
+++ src/Sections/Mission.js
@@ -3,9 +3,9 @@ import { Title, Button, Text } from '@mantine/core';
 
 const Mission = () => {
   return (
-    <div className="flex justify-center items-center mt-16 mb-10 w-full h-96 bg-blue-700">
+    <div className="flex justify-center items-center mt-16 mb-10 w-full h-96 bg-teal-500">
       <Title className="text-3xl text-center text-white">
-        <Text className="inline title text-3xl text-cyan-200">Teleprecept-Match</Text> is dedicated to making preceptorships <br /> easy and accessible for all Students and Preceptors.
+        <Text className="inline title text-3xl text-cyan-900">Teleprecept-Match</Text> is dedicated to making preceptorships <br /> easy and accessible for all Students and Preceptors.
       </Title>
     </div>
   );
